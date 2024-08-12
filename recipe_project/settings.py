0ca1f0a4b68b2afa967f2a9e75813585c21e6363
IcyAI <148020399+IcyAI@users.noreply.github.com>
2024-08-12 00:41:45 -0400

    update requirments.text

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -89,6 +89,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'recipe_app',
+#         'USER': 'your_user_name',
+#         'PASSWORD': 'your_password',
+#         'HOST': 'localhost',  # Use the PostgreSQL server address; 'localhost' for local setup
+#         'PORT': '5432',       # Default PostgreSQL port
+#     }
+# }
 
 
 # Password validation
