3bbb365c30750dfe7c6166e3700d11b885b1fcc5
IcyAI <148020399+IcyAI@users.noreply.github.com>
2024-08-12 13:52:24 -0400

    updated setting.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -133,17 +133,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR / 'static',
-    os.path.join(BASE_DIR, "media/recipes"),
     os.path.join(BASE_DIR, "recipes/static/recipes/css"),
     os.path.join(BASE_DIR, "recipes/static/recipes/images"),
 ]
