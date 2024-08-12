12d3a6ea18c82517974e686d1baf17bd3772d93a
IcyAI <148020399+IcyAI@users.noreply.github.com>
2024-08-12 10:52:52 -0400

    updated settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -81,17 +81,17 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.config(default='postgres://localhost')
+# }
 
 # DATABASES = {
 #     'default': {
